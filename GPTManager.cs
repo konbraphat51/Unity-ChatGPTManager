50020ed8822a7ca189363c5c5bfa16494af502db
konbraphat51 <101827492+konbraphat51@users.noreply.github.com>
2023-03-25 23:28:34 +0900

    Update GPTManager.cs

--- GPTManager.cs
+++ GPTManager.cs
@@ -123,53 +123,7 @@ namespace GPT
         }
 
         //以下、JSONデータのクラス
-
         [JsonObject]
-        private class APIRequestData
-        {
-            [JsonProperty("model")]
-            public string Model { get; set; } = "text-davinci-003";
-            [JsonProperty("prompt")]
-            public string Prompt { get; set; } = "";
-            [JsonProperty("temperature")]
-            public int Temperature { get; set; } = 0;
-            [JsonProperty("max_tokens")]
-            public int MaxTokens { get; set; } = 100;
-        }
-   
-        [JsonObject]
-        private class APIResponseData
-        {
-            [JsonProperty("id")]
-            public string Id { get; set; }
-            [JsonProperty("object")]
-            public string Object { get; set; }
-            [JsonProperty("model")]
-            public string Model { get; set; }
-            [JsonProperty("created")]
-            public int Created { get; set; }
-            [JsonProperty("choices")]
-            public ChoiceData[] Choices { get; set; }
-            [JsonProperty("usage")]
-            public UsageData Usage { get; set; }
-        }
-    
-        [JsonObject]
-        private class UsageData
-        {
-            [JsonProperty("prompt_tokens")]
-            public int PromptTokens { get; set; }
-            [JsonProperty("completion_tokens")]
-            public int CompletionTokens { get; set; }
-            [JsonProperty("total_tokens")]
-            public int TotalTokens { get; set; }
-        }
-    
-        [JsonObject]
-        private class ChoiceData
-        {
-            [JsonProperty("text")]
-            public [JsonObject]
         public class APIRequestData
         {
             [JsonProperty("model")]
